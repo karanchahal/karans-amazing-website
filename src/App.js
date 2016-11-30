ef36ae5130183b949598d29d9aa5245fa066e6f9
Karan <karanbleep@gmail.com>
2016-11-30 20:31:31 +0530

    Set up dev env

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const todos = [
+  {
+    task: 'make react tutorial'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+
+export default App;
