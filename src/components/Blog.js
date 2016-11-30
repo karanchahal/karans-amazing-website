3b9ca227b43716cc30b011035c022d7ba5bfcf0e
Karan <karanbleep@gmail.com>
2016-12-01 04:18:50 +0530

    Added pixyll styling

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,11 +5,31 @@ class Blog extends Component {
   render() {
     return (
       <div>
-      <h1>You are at a blog</h1>
-      <h2>Blog Posts</h2>
-      <ul>
-        <li> <Link to={`/post/testpost`}>This is a test </Link> </li>
-      </ul>
+        <div className="post p2 p-responsive wrap" role="main">
+          <div className="measure">
+            <div className="home">
+              <div className="posts">
+                <div className="post py3">
+                  <p className="post-meta">Oct 26, 2016</p>
+                  <Link to={`/post/testpost`} className="post-link"><h3 className="h1 post-title">Lets Make A Chrome Extension</h3></Link>
+                  <p className="post-summary">
+                      An introduction to extension programming wherein we make a torrent scrapping chrome extension !
+                  </p>
+                </div>
+              </div>
+              <div className="pagination clearfix mb1 mt4">
+                <div className="left">
+                  <span className="pagination-item disabled">Newer</span>
+                </div>
+                <div className="right">
+                  <a className="pagination-item" href="/page2">Older</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
       {this.props.children}
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,18 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <div className="ui three item menu">
-          <Link to={`/blog`} className="active item">
-            Blog
-          </Link>
-          <Link to={`/projects`} className="item">
-            Projects
-          </Link>
-          <Link to={`/about`} className="item">
-            About
-          </Link>
-
-        </div>
+        <header className="site-header px2 px-responsive">
+          <div className="mt2 wrap">
+            <div className="measure">
+              <Link to={`/blog`} className="site-title">Karan's Blog</Link>
+              <nav className="site-nav">
+                <Link to={`/about`}>About Me</Link>
+                <Link to={`/projects`}>Projects</Link>
+              </nav>
+              <div className="clearfix"></div>
+            </div>
+          </div>
+        </header>
         {this.props.children}
       </div>
     );
