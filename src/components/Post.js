9200d481f025f84a9a9f486511ed6cb828426001
Karan <karanbleep@gmail.com>
2016-12-04 03:07:14 +0530

    Removed infinite scroll, need to refactor massively for that

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import marked from 'marked'
 import axios from 'axios'
 
@@ -14,19 +15,16 @@ marked.setOptions({
 })
 
 
-let data = {
-  text: 'Greetings fellow human,We are going to make a simple chrome extension today.It is a very powerful concept that if used correctly can add a lot of improvements to web interaction on your browser. \n# The Idea\n We are going to make an extension that makes it easier to download torrent files. This extension aims to alleviate problems that a user encounters while downloading torrent files from various torrent hosting websites on the internet .These websites generally have a lot of spam and advertisements across them, which makes it difficult to find the link to the file you actually want to download.To solve this, we are going to make an extension that parses all magnet links on the page and open them on clicking the extension icon, as shown above. The extension should also go through a color change on detecting torrent magnet links on the current tab.',
-  heading: 'Lets Make A Chrome Extension',
-  date: 'Oct 26, 2016'
-}
 
 class Post extends Component {
 
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
     this.state = {'post':''}
+
   }
+
+
 
   componentWillMount() {
 
@@ -37,11 +35,6 @@ class Post extends Component {
     .catch(err => console.log(err));
   }
 
-  renderNewPost() {
-    if(window.scrollY/ (document.documentElement.scrollHeight - window.innerHeight)> 0.8) {
-      console.log('Load New Post')
-    }
-  }
 
   render() {
 
@@ -51,8 +44,8 @@ class Post extends Component {
         <div className="post p2 p-responsive wrap" role="main">
             <div className="measure">
                 <div className="post-header mb2">
-                  <h1>{data.heading}</h1>
-                  <span className="post-meta">{data.date}</span><br></br>
+                  <h1>Chrome Extension</h1>
+                  <span className="post-meta">01/01/1970</span><br></br>
 
                   <span className="post-meta small">
                     5 minute read
@@ -64,7 +57,6 @@ class Post extends Component {
           </div>
         </div>
 
-        {this.renderNewPost()}
       </div>
     );
   }
