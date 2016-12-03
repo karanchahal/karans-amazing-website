f5b587c364e6fc7028e505610fc83602a93733c8
Karan <karanbleep@gmail.com>
2016-12-03 18:19:10 +0530

    Added styling for about

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,28 @@ import React, { Component } from 'react';
 
 class About extends Component {
   render() {
+    let barStyle = {
+      opacity: 1,
+      transform: 'translate3d(0 px, 0px, 0px)',
+      color: '#ea9078'
+
+    };
     return (
-      <div id="centerContainer">
-        hey
-      </div>
+        <div className="post p2 p-responsive wrap" role="main">
+          <div className="measure">
+
+            <div className="hello">
+                <span id="intro">
+                  <span style={barStyle}>H</span>
+                  <span style={barStyle}>e</span>
+                  <span style={barStyle}>l</span>
+                  <span style={barStyle}>l</span>
+                  <span style={barStyle}>o</span>
+                </span>
+              </div>
+
+          </div>
+        </div>
     );
   }
 }
