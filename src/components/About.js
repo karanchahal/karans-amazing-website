9da86629529a57286d00b79f578cfc850b56e4ac
Karan <karanbleep@gmail.com>
2016-12-06 01:49:34 +0530

    Made components to handle letters in about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-
+import _ from 'lodash'
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'test': '<b>Hey</b>'
+    }
+  }
+  renderWord() {
+    let word = 'Hello.'
+    let colorz = [
+      '#e8c060',
+      '#dc6b6b',
+      '#65c3ad',
+      '#ea9078',
+      '#33afff'
+    ]
+    let cIndex = 0
+    return _.map(word,(letter,index) => {
+
+      cIndex = (cIndex+1)%colorz.length;
+      return <AboutLetter key={index} color={colorz[cIndex]} letter={letter} />
+    })
+
+  }
+
   render() {
+
     let barStyle = {
       opacity: 1,
       transform: 'translate3d(0 px, 0px, 0px)',
-      color: '#ea9078'
+      color: '#dc6b6b'
 
     };
     return (
+
         <div className="post p2 p-responsive wrap" role="main">
           <div className="measure">
             <div className="centeredContainer">
+
               <div className="hello">
                 <center>
+
                   <span id="intro">
-                    <span style={barStyle}>H</span>
-                    <span style={barStyle}>e</span>
-                    <span style={barStyle}>l</span>
-                    <span style={barStyle}>l</span>
-                    <span style={barStyle}>o</span>
+                  {this.renderWord()}
                   </span>
                   </center>
               </div>
@@ -31,4 +55,20 @@ class About extends Component {
   }
 }
 
+class AboutLetter extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    let barStyle = {
+      opacity: 1,
+      transform: 'translate3d(0 px, 0px, 0px)',
+      color: this.props.color
+    }
+    console.log(this.props.letter)
+    return ( <span style={barStyle}>{this.props.letter}</span>);
+  }
+}
 export default About;
